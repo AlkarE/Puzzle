f414e4f0ab34a0cc474301d9abf20a1b97adf045
alkar <alkar.ew@gmail.com>
2023-10-10 11:02:55 +0200

    feat: new props for theme

--- src/js/themes.js
+++ src/js/themes.js
@@ -6,7 +6,19 @@ export default function getThemes() {
         'C62300', 'AF1F00', '9B1B00',
         '8E1800', '841700', '7A1500',
         '721400', '651200', '591000',
-        '530F00', '490D00', '3A0A00'],
+        '530F00', '490D00', '3A0A00'
+      ],
+      border1: '5E2100',
+      border2: '984500',
+      border3: 'E83900',
+      btn: {
+        outerColorStart: '724B00',
+        outerColorEnd: '5F0900',
+        innerColorStart: '724A00',
+        innerColorEnd: '610B01',
+        border: '4F0000',
+        icon: 'E88B00',
+      }
 
     },
     green: {
@@ -15,7 +27,18 @@ export default function getThemes() {
         '007600', '006900', '005D00',
         '005D00', '005000', '004800',
         '004800', '003900', '002E00'
-      ]
+      ],
+      border1: '005500',
+      border2: '008B00',
+      border3: '00B800',
+      btn: {
+        outerColorStart: '019C01',
+        outerColorEnd: '003100',
+        innerColorStart: '009B00',
+        innerColorEnd: '013501',
+        border: '003100',
+        icon: '27CCB0',
+      }
     },
     violet: {
       partsColor: [
@@ -23,7 +46,18 @@ export default function getThemes() {
         '6200BA', '5B00AD', '5500A1',
         '5500A1', '4E0094', '49008C',
         '49008C', '41007D', '3A0070'
-      ]
+      ],
+      border1: '5912B5',
+      border2: '822ACF',
+      border3: '857ECF',
+      btn: {
+        outerColorStart: '7B23E0',
+        outerColorEnd: '411278',
+        innerColorStart: '7A22DE',
+        innerColorEnd: '421279',
+        border: '16003B',
+        icon: 'ABB8FF',
+      }
     },
     blue: {
       partsColor: [
@@ -31,7 +65,18 @@ export default function getThemes() {
         '0051BA', '004CAD', '0047A1',
         '0047A1', '004194', '003E8C',
         '003E8C', '00387D', '003270'
-      ]
+      ],
+      border1: '1258B5',
+      border2: '2A70CF',
+      border3: '36A9E1',
+      btn: {
+        outerColorStart: '238AE0',
+        outerColorEnd: '114977',
+        innerColorStart: '238AE0',
+        innerColorEnd: '114A78',
+        border: '002A3B',
+        icon: '73B0FF',
+      }
     }
   }
 }
